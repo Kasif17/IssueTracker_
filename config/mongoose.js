7fa0fb29c31fcea903ea3b634a9bb698d7091ad9
Kasif17 <developer786kasif@gmail.com>
2023-05-24 21:37:33 +0530

    second comm.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://rajdeep007:singh%40123@issue-tracker.pr1xw5f.mongodb.net/Issue-Tracker?retryWrites=true&w=majority';
+//const url = 'mongodb+srv://rajdeep007:singh%40123@issue-tracker.pr1xw5f.mongodb.net/Issue-Tracker?retryWrites=true&w=majority';
+const url ='mongodb+srv://developer786kasif:Coding%40786@cluster0.mfz18jl.mongodb.net/Issue1?retryWrites=true&w=majority';
 // mongoose.connect('url') 
 mongoose.connect(url);
 const db=mongoose.connection;
